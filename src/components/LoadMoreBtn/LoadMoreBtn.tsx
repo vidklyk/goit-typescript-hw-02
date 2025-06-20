82ed564f4b4027b8efde8da3788964b8079a38f3
Dmytro <tsaplincinema@gmail.com>
2025-06-20 19:52:19 +1000

    hw2-typescript fix build

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,4 +1,8 @@
-export default function LoadMoreBtn({ onClick }) {
+type Props = {
+  onClick: () => void;
+};
+
+export default function LoadMoreBtn({ onClick }: Props) {
   return (
     <div style={{ textAlign: "center", margin: "20px" }}>
       <button onClick={onClick}>Load more</button>
